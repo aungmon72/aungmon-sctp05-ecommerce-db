396b59a263f326687ea516c8a19e55cb0a5d9b5b
aungmon72 mon <aungmon72@yahoo.com>
2025-01-22 03:24:04 +0000

    20250122B

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,8 @@ const userService = require('../services/userService');
 const jwt = require('jsonwebtoken');
 
 // POST register a new user
-router.post('/register', async (req, res) => {
+router.post('api/register', async (req, res) => {
+  console.log(req.body);
   try {
     const {
       name,
